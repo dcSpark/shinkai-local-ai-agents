976833c1be005fa1f963b74be1a98974e686bf5c
paulclindo <paulclindo@gmail.com>
2025-09-24 13:27:43 -0300

    upgrading zod

--- libs/shinkai-node-state/src/forms/auth/quick-connection.ts
+++ libs/shinkai-node-state/src/forms/auth/quick-connection.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const quickConnectFormSchema = z.object({
-  node_address: z.string().url({
-    message: 'Node Address must be a valid URL',
+  node_address: z.url({
+    error: 'Node Address must be a valid URL',
   }),
 });
 
